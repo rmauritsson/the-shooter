78b6f992d5e020102ef310a04cbde5b96e36dae4
Leo <ronald.mauritsson@gmail.com>
2020-05-26 14:28:14 +0300

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'index.html'),
+          to: path.resolve(__dirname, 'dist') 
+        }
+      ]
+    })
+  ]
+}
