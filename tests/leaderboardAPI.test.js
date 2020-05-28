77fee57b499e50036d3fb441fe3f6512590cfe89
Leo <ronald.mauritsson@gmail.com>
2020-05-28 12:19:23 +0300

    Added tests for all the functions

--- tests/leaderboardAPI.test.js
+++ tests/leaderboardAPI.test.js
@@ -1,8 +1,18 @@
 import LeaderboardAPI from '../src/leaderboardAPI';
+import Store from '../src/store';
+import Display from '../src/display';
 
 jest.mock('../src/leaderboardAPI');
 
 const mock = jest.fn((user) => LeaderboardAPI.showResults(user, score));
+
+const mock2 = jest.fn((user, score) => LeaderboardAPI.showResults(user, score));
+
+test('Returns scores', () => {
+  Promise.resolve(mock2('Leonard', '350')).then(({ result }) => {
+    expect(result).toBe('1. Leonard: 350');
+  }).catch(() => {});
+});
 
 test('Returns reject message asking user to provide score when it is not given', () => {
   Promise.resolve(mock).catch((error) => {
@@ -24,4 +34,11 @@ test('Returns reject message asking user to provide username when username not p
       'You need to provide a valid user for the score',
     );
   });
+});
+
+test('Expects undefined since the function does not return anything', () => {
+  const jsdomAlert = window.alert;
+  window.alert = () => {};
+  expect(Store.remove('Harry')).toBeUndefined();
+  window.alert = jsdomAlert;
 });
